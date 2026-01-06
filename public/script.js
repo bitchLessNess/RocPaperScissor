0844fa420bbc3420a5cab9721d617ec34c9b3044
bitchLessNess <tipoftech2022@gmail.com>
2026-01-06 05:44:33 +0530

    Completed the design

--- public/script.js
+++ public/script.js
@@ -2,13 +2,22 @@ const rock = document.getElementById("rockBtn")
 const paper = document.getElementById("paperBtn")
 const scissor = document.getElementById("scissorBtn")
 const buttons = document.querySelectorAll(".btn")
+const playButtons = document.getElementById("buttons");
 const result = document.getElementById("result");
 const resText = document.getElementById("resText")
+const main = document.querySelector("main");
 
 const scoreCard = document.getElementById("scoreBoard")
+const compChoice = Math.floor(Math.random() * 3 + 1);
 
-let count = 0;
+var playerCount = 0;
+var compCount = 0;
+let playerChoice;
 
+
+
+let totalClicks = 0;
+const MAX_CLICKS = 3;
 
 
 
@@ -21,12 +30,11 @@ const res = () => {
     }, 3000);
     setTimeout(() => {
         result.classList.add("hidden")
-    }, 4000);
+    }, 5000);
 
 }
 
 
-const compChoice = Math.floor(Math.random() * 3 + 1);
 
 function unhideComp() {
 
@@ -49,7 +57,7 @@ function unhideComp() {
         else {
             document.getElementById("image3").classList.add("hidden")
         }
-    }, 3000)
+    }, 2000)
 }
 
 function unhidePlayer(isClicked) {
@@ -73,64 +81,56 @@ function unhidePlayer(isClicked) {
         else if (isClicked == "scissor") {
             document.getElementById("player1sicssor").classList.add("hidden")
         }
-    }, 3000)
+    }, 2000)
 }
 
 
-// hide button 
-// function hideBtn() {
-//   buttons.forEach(btn => {
-//     btn.classList.add("hidden2");
-//   });
 
-//   // show after 5 seconds
-//   setTimeout(() => {
-//     buttons.forEach(btn => {
-//       btn.classList.remove("hidden2");
-//     });
-//   }, 5000);
-// }
 
 buttons.forEach(btn => {
-  btn.addEventListener("click", () => {
-    // hide all buttons
-    buttons.forEach(b => b.classList.add("hidden2"));
+    btn.addEventListener("click", () => {
+        // hide all buttons
+        buttons.forEach(b => b.classList.add("hidden2"));
 
-    // show again after 5 seconds
-    setTimeout(() => {
-      buttons.forEach(b => b.classList.remove("hidden2"));
-    }, 5000);
-  });
+        // show again after 5 seconds
+        setTimeout(() => {
+            buttons.forEach(b => b.classList.remove("hidden2"));
+        }, 4500);
+    });
 });
 
 // Rock : when the player chooses the rock button 
 rock.addEventListener('click', () => {
     console.log("click")
     isClicked = "rock";
-
+    playerChoice = 1;
     unhideComp()
     unhidePlayer(isClicked)
-   
+
     res()
     if (compChoice === 1) { resText.innerText = "It's a Draw" }
-    else if (compChoice === 2) { resText.innerText = "You lost to paper" }
-    else { resText.innerText = "You Won!!" }
-
+    else if (compChoice === 2) { resText.innerText = "You lost to paper"; compCount++; }
+    else {
+        resText.innerText = "You Won!!";
+         playerCount++;
+    }
+    console.log("player:- ", playerCount, " Comp:- ", compCount)
 })
 
 // paper : when the player chooses the rock button 
 paper.addEventListener('click', () => {
     console.log("click")
     isClicked = "paper";
+    playerChoice = 2;
 
     unhideComp()
     unhidePlayer(isClicked)
-   
-    res()
-    if (compChoice === 1) { resText.innerText = "You won!!" }
-    else if (compChoice === 2) { resText.innerText = "It's a Draw" }
-    else { resText.innerText = "You lost to Scissor" }
 
+    res()
+    if (compChoice === 1) { resText.innerText = "You won!!";  playerCount++; }
+    else if (compChoice === 2) { resText.innerText = "It's a Draw" }
+    else { resText.innerText = "You lost to Scissor";  compCount++; }
+    console.log("player:- ", playerCount, " Comp:- ", compCount)
 
 })
 
@@ -138,13 +138,68 @@ paper.addEventListener('click', () => {
 scissor.addEventListener('click', () => {
     console.log("click scissor")
     isClicked = "scissor";
+    playerChoice = 3;
 
     unhideComp()
     unhidePlayer(isClicked)
-    
-    res()
-    if (compChoice === 1) { resText.innerText = "You Lost to Rock" }
-    else if (compChoice === 2) { resText.innerText = "You Won!!" }
-    else { resText.innerText = "It's a Draw" }
 
+    res()
+    if (compChoice === 1) { resText.innerText = "You Lost to Rock";  compCount++; }
+    else if (compChoice === 2) { resText.innerText = "You Won!!";  playerCount++; }
+    else { resText.innerText = "It's a Draw" }
+    console.log("player:- ", playerCount, " Comp:- ", compCount)
+})
+
+/// funtction to update the result board
+ function finalResult(){
+    document.getElementById("playerScore").innerText = playerCount;
+    document.getElementById("compScore").innerText = compCount;
+    if(compCount > playerCount){ document.getElementById("finalResult").innerText = "Computer Won The Game"}
+    else if(playerCount >compCount){document.getElementById("finalResult").innerText = "Player Won The Game"}
+    else{
+        document.getElementById("finalResult").innerText = "The Game Draws"   
+    }
+ }
+
+
+//// button ivisiblity matrixx
+buttons.forEach(btn => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(e.target)
+    
+    if (totalClicks >= MAX_CLICKS) return;
+
+    totalClicks++;
+
+    console.log("Round:", totalClicks);
+
+    // 👉 game logic here
+
+    if (totalClicks === MAX_CLICKS) {
+      console.log("Game Over");
+      setTimeout(() => {
+        main.classList.add("hidden")
+       playButtons.classList.add("hidden");
+       scoreCard.classList.remove("hidden");
+      }, 4000);
+        finalResult();
+        console.log("final result updated")
+
+    }
+  });
+ 
+});
+
+
+// result updation 
+const playAgain = document.getElementById("playAgain");
+
+playAgain.addEventListener('click', ()=>{
+    scoreCard.classList.add("hidden")
+            main.classList.remove("hidden")
+       playButtons.classList.remove("hidden");
+       playerCount = 0;
+       compCount = 0;
+       totalClicks = 0;
 })
